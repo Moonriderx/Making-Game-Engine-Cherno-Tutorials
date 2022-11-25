ee1a59b607ed005734e3a5a5410b90bb99d22a86
Luben Lubenov <Luben1993@abv.bg>
2022-11-25 16:59:39 +0200

    Fixed project folders

---
+++ ChernoGameEngineTutorialLocal/ChernoGameEngineTutorialLocal/src/Corelibrary.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// For use by the Game Engine applications 
+
+#include "CoreLibrary/Application.h"
---
+++ ChernoGameEngineTutorialLocal/Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,25 @@
+#include "Corelibrary.h"
+
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox()
+	{
+
+	}
+	~Sandbox()
+	{
+
+	}
+};
+
+int main()
+{
+	Sandbox* sandbox = new Sandbox();
+	sandbox->Run();
+    
+
+
+	delete sandbox;
+}
